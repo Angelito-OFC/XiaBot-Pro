42578e129e88ed874fd390a6e09bff6c8e373ed7
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-27 14:27:00 -0400

    Actualizar downloader-tiktokstalk.js

--- plugins/downloader-tiktokstalk.js
+++ plugins/downloader-tiktokstalk.js
@@ -1,28 +1,27 @@
-import fetch from 'node-fetch';
-const handler = async (m, {conn, text}) => {
-  if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚃𝙸𝙺𝚃𝙾𝙺*', m);
-  try {
-    const res = await fetch(`https://api.lolhuman.xyz/api/stalktiktok/${text}?apikey=${lolkeysapi}`);
-    const res2 = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=${lolkeysapi}`;
-    const json = await res.json();
-    if (res.status !== 200) throw await res.text();
-    if (!json.status) throw json;
-    const thumb = await (await fetch(json.result.user_picture)).buffer();
-    const Mystic = `
-*𝚄𝚂𝚄𝙰𝚁𝙸𝙾:* ${json.result.username}
-*𝙽𝙾𝙼𝙱𝚁𝙴:* ${json.result.nickname}
-*𝚂𝙴𝙶𝚄𝙸𝙳𝙾𝚁𝙴𝚂:* ${json.result.followers}
-*𝚂𝙴𝙶𝚄𝙸𝙳𝙾𝚂:* ${json.result.followings}
-*𝙻𝙸𝙺𝙴𝚂:* ${json.result.likes}
-*𝚅𝙸𝙳𝙴𝙾𝚂:* ${json.result.video}
-*𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽:* ${json.result.bio}
-`.trim();
-    conn.sendFile(m.chat, res2, 'error.jpg', Mystic, m, false);
-  } catch (e) {
-    throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙽𝙾 𝚂𝙴 𝙴𝙲𝙾𝙽𝚃𝚁𝙾 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙸𝙽𝙶𝚁𝙴𝚂𝙰𝙳𝙾*';
-  }
-};
-handler.help = ['tiktokstalk'].map((v) => v + ' <username>');
-handler.tags = ['stalk'];
-handler.command = /^(tiktokstalk|ttstalk)$/i;
-export default handler;
+
+import fg from 'api-dylux'
+let handler = async (m, { conn, text, args }) => {
+
+  if (!text) throw `✳️ Ingrese el Username de un usuario de TikTok`
+try {
+  let res = await fg.ttStalk(args[0])
+  let txt = `
+┌──「 *TIKTOK STALK* 
+▢ *🔖Nombre:* ${res.name}
+▢ *🔖Username:* ${res.username}
+▢ *👥Seguidores:* ${res.followers}
+▢ *🫂Siguiendo:* ${res.following}
+▢ *📌Desc:* ${res.desc}
+
+▢ *🔗 Link* : https://tiktok.com/${res.username}
+└────────────`
+  await conn.sendFile(m.chat, res.profile, 'tt.png', txt, m)
+} catch {
+    m.reply(`✳️ Revisa que el nombre de usuario sea de TikTok`)
+}
+}
+handler.help = ['tiktokstalk']
+handler.tags = ['dl']
+handler.command = /^t(tstalk|iktokstalk)$/i
+
+export default handler
