d4439c687da853fc3eaf7a7e1db86ac02dad2b76
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-26 21:46:49 -0400

    Actualizar plugins

---
+++ plugins/downloader-tiktok-mp3.js
@@ -0,0 +1,11 @@
+import { tiktokdl } from '../lib/tiktok.js'
+let handler = async (m, { conn, args, text, usedPrefix, command }) => { 
+  if (!args[0]) throw `*ejemplo:* ${usedPrefix+command} https://vm.tiktok.com/ZMME5XBt3/`;
+  let data = await tiktokdl(text);
+  await conn.sendMessage(m.chat, { react: { text: "â�³", key: m.key } });
+conn.sendMessage(m.chat, { audio: { url: data.originalVideoUrl }, mimetype: 'audio/mpeg', fileName: `${data.title}.mp3` }, { quoted: m })
+}
+handler.help = ['tiktokmp3', 'tiktokaudio'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tiktokaudio|tiktokmp3|)$/i
+export default handler
