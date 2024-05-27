409aa2316889bf656c03af14a0a4cdf5921a3bb7
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-27 14:29:46 -0400

    Actualizar rpg-myns.js

--- plugins/rpg-myns.js
+++ plugins/rpg-myns.js
@@ -1,10 +1,8 @@
 import {createHash} from 'crypto';
 const handler = async function(m, {conn, text, usedPrefix}) {
   const sn = createHash('md5').update(m.sender).digest('hex');
-  m.reply(`┏┅ ━━━━━━━━━━━━ ┅ ━
-┃ *𝙽𝚄𝙼𝙴𝚁𝙾 𝙳𝙴 𝚂𝙴𝚁𝙸𝙴:* 
-┃ ${sn}
-┗┅ ━━━━━━━━━━━━ ┅ ━`.trim());
+  m.reply(`🔮 𝙀𝙎𝙀 𝙀𝙎 𝙎𝙐 𝙉𝙐𝙈𝙀𝙍𝙊 𝘿𝙀 𝙎𝙀𝙍𝙄𝙀 🔮`.trim());
+await m.reply(`${sn}`.trim())
 };
 handler.help = ['myns'];
 handler.tags = ['xp'];
