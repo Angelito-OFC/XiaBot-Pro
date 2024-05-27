714f996350b33dde8902486688904a03e297a1d8
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-26 21:14:43 -0400

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,7 +5,8 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     throw false;
   }
   const pesan = args.join` `;
-  const oi = `*${participants.length} 𝙈𝙞𝙚𝙢𝙗𝙧𝙤𝙨* ${pesan}`;
+const oi = `✉️ 𝙈𝙀𝙉𝙎𝘼𝙅𝙀 ${pesan}`;
+const oi = `👤 𝙈𝙄𝙀𝙈𝘽𝙍𝙊𝙎 *${participants.length}`;
   let teks = `🔮 𝙓𝙄𝘼 𝘽𝙊𝙏 𝙇𝙊𝙎 𝙄𝙉𝙑𝙊𝘾𝘼\n\n ${oi}\n\n┏・🔮 𝙧𝙚𝙫𝙞𝙫𝙖𝙣 𝙘𝙪𝙚𝙧𝙣𝙪𝙙@𝙨 !\n`;
   for (const mem of participants) {
     teks += `┣・🔮 @${mem.id.split('@')[0]}\n`;
