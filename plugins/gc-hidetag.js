23e889fb3d9989d9a52d1cba2e2acf8d684419fb
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-26 21:54:55 -0400

    Actualizar gc-hidetag.js

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -36,7 +36,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
     }
   }
 };
-handler.command = /^(hidetag|notificar|notify|viso)$/i;
+handler.command = /^(hidetag|notificar|notify|n)$/i;
 handler.group = true;
 handler.admin = true;
 export default handler;
