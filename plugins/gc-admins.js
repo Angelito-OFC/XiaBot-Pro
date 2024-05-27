58960c2f8fff3e880f83df0d5ed075d5a05d00e4
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-26 22:08:10 -0400

    Actualizar gc-admins.js

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -5,7 +5,7 @@ const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let pesan = args.join` `
 let oi = `✉️ 𝙈𝙀𝙉𝙎𝘼𝙅𝙀: ${pesan}\n👤 𝘼𝘿𝙈𝙄𝙉𝙎: _*${participants.length}*_`
-let text = `𝘼𝘿𝙈𝙄𝙉𝙎 𝙍𝙀𝙑𝙄𝙑𝘼𝙉
+let text = `𝘼𝘿𝙈𝙄𝙉𝙎 𝙍𝙀𝙑𝙄𝙑𝘼𝙉\n\n
 ${oi}
 
 ・🔮𝘼𝙙𝙢𝙞𝙣𝙨 𝙙𝙚𝙡 𝙜𝙧𝙪𝙥𝙤:
