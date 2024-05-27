b3937393d905cb505a131317eb9712bf17181bf5
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-27 15:02:25 -0400

    Actualizar rpg-cofre.js

--- plugins/rpg-cofre.js
+++ plugins/rpg-cofre.js
@@ -19,7 +19,7 @@ const handler = async (m, {isPrems, conn}) => {
 ║┈┈┈┈┈┈┈┈┈┈┈┈┈
 ║➢ *${dia} Diamantes* 💎
 ║➢ *${tok} Tokens* 🪙
-║➢ *${mystic} MichiCoins* 👾
+║➢ *${mystic} XiaCoins* 👾
 ║➢ *${expp} Exp* ⚡
 ╚═════════════════⬣`;
 
