ffe6cba8d1509fd5e5c4454dbea9b1af57007de7
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-26 23:13:37 -0400

    Actualizar gc-config.js

--- plugins/gc-config.js
+++ plugins/gc-config.js
@@ -7,6 +7,7 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
     'abrir': 'not_announcement',
     'cerrar': 'announcement',
   }[(args[0] || '')];
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
   if (isClose === undefined) {
     throw `
 ‎ ‎ ‎ ‎ ‎ ‎ ❱❱ 𝘼  𝙑  𝙄  𝙎  𝙊 ❰❰
