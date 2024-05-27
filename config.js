83b4eab6a5d4bfff8f1a84034e6016e10c5f4f13
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-26 21:01:42 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -36,31 +36,6 @@ global.owner = [
 global.suittag = ['59168683798'];
 global.prems = ['59168683798'];
 
-global.APIs = {
-  xteam: 'https://api.xteam.xyz',
-  dzx: 'https://api.dhamzxploit.my.id',
-  lol: 'https://api.lolhuman.xyz',
-  violetics: 'https://violetics.pw',
-  neoxr: 'https://api.neoxr.my.id',
-  zenzapis: 'https://zenzapis.xyz',
-  akuari: 'https://api.akuari.my.id',
-  akuari2: 'https://apimu.my.id',
-  nrtm: 'https://fg-nrtm.ddns.net',
-  bg: 'http://bochil.ddns.net',
-  fgmods: 'https://api-fgmods.ddns.net'
-};
-
-// Claves de API
-
-global.APIKeys = {
-  'https://api.xteam.xyz': 'd90a9e986e18778b',
-  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
-  'https://api.neoxr.my.id': `${keysneoxr}`,
-  'https://violetics.pw': 'beta',
-  'https://zenzapis.xyz': `${keysxxx}`,
-  'https://api-fgmods.ddns.net': 'fg-dylux'
-};
-
 global.packname = '𝙓𝙞𝙖𝘽𝙤𝙩-𝙈𝘿 🔮';
 global.author = '𝙓𝙞𝙖𝘽𝙤𝙩-𝙈𝘿 🔮';
 global.wm = '𝙓𝙞𝙖𝘽𝙤𝙩-𝙈𝘿 🔮';
