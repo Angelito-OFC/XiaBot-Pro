805fb5ef6282bf70d1b478f1b5647345715898cd
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-27 21:37:37 -0400

    Actualizar agenda-semanal.js

--- plugins/agenda-semanal.js
+++ plugins/agenda-semanal.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
-let url = agenda[Math.floor(Math.random() * pies.length)]
+let url = agenda[Math.floor(Math.random() * agenda.length)]
 conn.sendFile(m.chat, url, 'error.jpg', `*🔮 AGENDA SEMANAL 🔮*`, m)
 //conn.sendButton(m.chat, `🔮 AGENDA SEMANAL 🔮`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
 }
