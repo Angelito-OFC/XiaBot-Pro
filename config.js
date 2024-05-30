b132e2fc8bb23abbf84254e89f7ba7858898afee
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-29 22:42:32 -0400

    Actualizar config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ global.imagen1 = fs.readFileSync('./Menu2.jpg');
 global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
 global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
 global.imagen4 = fs.readFileSync('./Menu.png');
-global.imagen5 = fs.readFileSync('./src/+18.jpg');
+global.imagen5 = fs.readFileSync('./src/+18-hot.jpg');
 global.imagen6 = fs.readFileSync('./Menu3.png');
 
 global.mods = [];
