c788d50e0e60a2a3de250363e39c1af7436f846b
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-30 13:08:29 -0400

    Actualizar gc-guerradeclanes.js

--- plugins/gc-guerradeclanes.js
+++ plugins/gc-guerradeclanes.js
@@ -1,4 +1,5 @@
 let handler = async (m, { conn, participants, groupMetadata }) => { 
+const pp = imagen4;
 const groupAdmins = participants.filter(p => p.admin) 
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
