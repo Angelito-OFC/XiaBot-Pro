d6cdcc9aa75fe041e7829de48cb3a21ab2f831de
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-30 13:27:22 -0400

    Actualizar gc-scrims.js

--- plugins/gc-scrims.js
+++ plugins/gc-scrims.js
@@ -1,13 +1,12 @@
 let handler = async (m, { conn, participants, groupMetadata }) => {
-const pp = imagen2;
-const msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, {[m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : {text: '' || c}}, {quoted: m, userJid: conn.user.id}), text || q.text, conn.user.jid, {mentions: users}); 
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
 const groupAdmins = participants.filter(p => p.admin) 
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let text = 
 `
 ╭─────────⚔────────╮
-ㅤ𝐒𝐂𝐑𝐈𝐌𝐒 𝐂𝐎𝐌𝐏𝐄𝐓𝐈𝐓𝐈𝐕𝐎
+ㅤ 𝐒𝐂𝐑𝐈𝐌𝐒 𝐂𝐎𝐌𝐏𝐄𝐓𝐈𝐓𝐈𝐕𝐎
 ╰─────────⚔────────╯
 ╭───────────────────╮
 │ㅤ⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎 
@@ -17,7 +16,7 @@ let text =
 │➥ 𝐂𝐀𝐒𝐈𝐋𝐋𝐀:
 │➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
 │
-│     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 
+│     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 𝟏
 │
 │🔮 ➤ 
 │🔮 ➤  
@@ -26,15 +25,29 @@ let text =
 │
 │ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
 │🔮 ➤  
-│🔮 ➤
+│
+│
+│➥ 𝐓𝐎𝐓𝐀𝐋 𝐒𝐀𝐋𝐀𝐒:
+│➥ 𝐂𝐀𝐒𝐈𝐋𝐋𝐀:
+│➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
+│
+│     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 𝟐
+│
+│🔮 ➤ 
+│🔮 ➤  
+│🔮 ➤ 
+│🔮 ➤ 
+│
+│ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄:
+│🔮 ➤  
 ╰───────────────────╯
 `.trim()
-  
+
 await conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })   
 //await conn.sendButton(m.chat, wm, text, pp, [[lenguajeGB.smsGI10(), '.on'], [lenguajeGB.smsConMenu(), '/menu']], m, { mentions: [...groupAdmins.map(v => v.id), owner] })
 }
 handler.help = ['infogrup']
 handler.tags = ['group']
-handler.command = /^(scrim|scrimm)$/i
+handler.command = /^(scrim|scrim1)$/i
 handler.group = true
 export default handler
