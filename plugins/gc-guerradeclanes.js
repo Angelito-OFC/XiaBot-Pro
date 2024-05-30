dfa78d453f5934778fed6c3cddbddd1bb9a27590
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-29 23:09:13 -0400

    Actualizar plugins

---
+++ plugins/gc-guerradeclanes.js
@@ -0,0 +1,77 @@
+let handler = async (m, { conn, participants, groupMetadata }) => {
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
+const groupAdmins = participants.filter(p => p.admin) 
+const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
+const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
+let text = 
+`
+╭──────>⋆☽⋆ ⋆☾⋆<──────╮
+ㅤ   GUERRA DE CLANES
+        ${groupMetadata.subject}
+╰──────>⋆☽⋆ ⋆☾⋆<──────╯
+╭──────────────╮
+│ㅤ⏱ 𝐇𝐎𝐑𝐀𝐑𝐈𝐎 
+│➥ 
+│➥ 𝐉𝐔𝐆𝐀𝐃𝐎𝐑𝐄𝐒:
+│
+│     𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 ➹𝟏
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│     
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 ➹𝟐
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 ➹𝟑
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 ➹𝟒
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 ➹𝟓
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│    𝗘𝗦𝗖𝗨𝗔𝗗𝗥𝗔 ➹𝟔
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│ㅤʚ 𝐒𝐔𝐏𝐋𝐄𝐍𝐓𝐄𝐒:
+│⚜️ ➤ 
+│⚜️ ➤
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+╰─────────────╯
+
+`.trim()
+  
+await conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })   
+//await conn.sendButton(m.chat, wm, text, pp, [[lenguajeGB.smsGI10(), '.on'], [lenguajeGB.smsConMenu(), '/menu']], m, { mentions: [...groupAdmins.map(v => v.id), owner] })
+}
+handler.help = ['infogrup']
+handler.tags = ['group']
+handler.command = /^(guerra|guerradeclanes|info(gro?up))$/i
+handler.group = true
+export default handler
