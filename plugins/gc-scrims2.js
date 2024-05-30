1fd5b8de4a86d19d45bffa3ec0b68fa3309a51c7
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-30 13:28:29 -0400

    Actualizar gc-scrims2.js

--- plugins/gc-scrims2.js
+++ plugins/gc-scrims2.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, participants, groupMetadata }) => {
-const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/grupos.jpg' 
+const pp = imagen4; 
 const groupAdmins = participants.filter(p => p.admin) 
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
