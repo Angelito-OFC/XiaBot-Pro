db7dc17bd3230e29edd4751e850b068aab613bdc
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-30 17:56:27 -0400

    Actualizar downloader-mega.js

--- plugins/downloader-mega.js
+++ plugins/downloader-mega.js
@@ -3,7 +3,7 @@ import path from "path";
 
 let handler = async (m, { conn, args, usedPrefix, text, command }) => {
     try {
-        if (!text) return m.reply(`${usedPrefix + command} https://mega.nz/file/ovJTHaQZ#yAbkrvQgykcH_NDKQ8eIc0zvsN7jonBbHZ_HTQL6lZ8`);
+        if (!text) return m.reply(`${usedPrefix + command} https://mega.nz/file/WHBRRQKB#sSD-5kxk7caGxG9v4Zc3ArMRBJy3E34ykFZyHu7PADU`);
 
         const file = File.fromURL(text);
         await file.loadAttributes();
