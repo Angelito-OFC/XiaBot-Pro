95de4b12b85d12eb1ac56e4c3b4f8c19f420a6d7
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-29 20:29:21 -0400

    Actualizar menu-ayuda.js

--- plugins/menu-ayuda.js
+++ plugins/menu-ayuda.js
@@ -25,24 +25,24 @@ let msg = generateWAMessageFromContent(m.chat, {
           nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
             buttons: [
               {
-                "name": "quick_reply",
+                "name": "🔮 MENU COMPLETO",
                 "buttonParamsJson": "{\"display_text\":\".menu\",\"id\":\"message\"}"
               },
               {
                 "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\"..menuff\",\"id\":\"message\"}"
+                "buttonParamsJson": "{\"display_text\":\".menuff\",\"id\":\"message\"}"
               },
               {
                 "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\"..menuowner\",\"id\":\"message\"}"
+                "buttonParamsJson": "{\"display_text\":\".menuowner\",\"id\":\"message\"}"
               },
               {
                 "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\"..labiblia\",\"id\":\"message\"}"
+                "buttonParamsJson": "{\"display_text\":\".labiblia\",\"id\":\"message\"}"
               },
               {
                 "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\"..menuanime\",\"id\":\"message\"}"
+                "buttonParamsJson": "{\"display_text\":\".menuanime\",\"id\":\"message\"}"
               },
               {
                 "name": "quick_reply",
