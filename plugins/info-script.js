e7d3cf41364d48f04764aa02ed41074f05566952
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-30 17:22:50 -0400

    Actualizar info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,18 +1,23 @@
-import moment from 'moment-timezone';
-import fetch from 'node-fetch';
-const handler = async (m, { conn, args }) => {
-   const res = await fetch('https://api.github.com/repos/Karim-off/XiaBot-Pro');
-   const json = await res.json();
-   let txt = `           *乂  B O T  -  S C R I P T  乂*\n\n`;
-      txt += `◦  *Nombre:* ${json?.name || 'TheMystic-Bot-MD'}\n`;
-      txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
-      txt += `◦  *Tamaño:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
-      txt += `◦  *Actualización:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n`;
-      txt += `◦  *Url:* ${json?.html_url || 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'}\n\n`;
-      txt += `	 ${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues`;
-   await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: m});
-};
-handler.help = ['scbot'];
-handler.tags = ['info'];
-handler.command = /^(sc|scbot|scrip|script)$/i;
-export default handler;
+import moment from 'moment-timezone'
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, args }) => {
+   let res = await fetch('https://api.github.com/repos/Karim-off/XiaBot-Pro')
+   let json = await res.json()
+   let txt = `*B O T  -  S C R I P T*\n\n`
+      txt += `        ◦  *Nombre* : ${json.name}\n`
+      txt += `        ◦  *Visitas* : ${json.watchers_count}\n`
+      txt += `        ◦  *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
+      txt += `        ◦  *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
+      txt += `        ◦  *Url* : ${json.html_url}\n`
+      txt += `        ◦  *Forks* : ${json.forks_count}\n`
+      txt += `        ◦  *Stars* : ${json.stargazers_count}`
+   await conn.sendFile(m.chat, 'https://telegra.ph/file/9468197456d862aae9387.jpg', m)
+
+}
+
+handler.help = ['script']
+handler.tags = ['main']
+handler.command = ['script', 'sc']
+handler.register = true 
+export default handler
