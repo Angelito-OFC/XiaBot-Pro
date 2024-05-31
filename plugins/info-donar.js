282bb6cf147eef45de1618dcde53e7d556960259
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-30 22:04:15 -0400

    Actualizar info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -1,14 +1,19 @@
 var handler = async (m, { conn, command }) => {
 
-let str = `🔮 𝘿 𝙊 𝙉 𝘼 𝙍
+let str = `🔮 𝘿 𝙊 𝙉 𝘼 𝙍  𝙓 𝙄 𝘼 𝙋 𝙍 𝙊
 
 » 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈
-${xbig}
+${ig}
 
 » 𝘾𝙍𝙀𝘼𝘿𝙊𝙍 𝘿𝙀𝙇 𝘽𝙊𝙏
 wa.me/59897246324
 
 _Puedes apoyar el bot dando tu estrellita en nuestro repositorio_
+
+» 𝙍𝙀𝙋𝙊𝙎𝙄𝙏𝙊𝙍𝙄𝙊
+${github}
+
+» 𝙂𝙍𝙐𝙋𝙊 𝙊𝙁𝘾
 ${md}`
 
 conn.reply(m.chat, str, m, fake, )
