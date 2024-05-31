a5cf9647042d41209441d97a2ef668116b7f00ab
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-30 22:10:05 -0400

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -11,8 +11,8 @@ let bio = biografia.status?.toString() || 'Sin Biografía'
 let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 
 await conn.sendContactArray(m.chat, [
-[devnum, `${await conn.getName('59168683798'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `🇧🇴 Bolivia`, `https://www.youtube.com/@Azami_YT`, bio],
-[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 CuriosityBot-MD`, `📵 No Hacer Spam`, email, `🇲🇽 México`, `https://github.com/AzamiJs/CuriosityBot-MD`, biobot]
+[devnum, `${await conn.getName('59168683798'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `🇧🇴 Bolivia`, `https://www.instagram.com/usxr_angelito`, bio],
+[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 XiaBot-Pro`, `📵 No Hacer Spam`, email, `🇦🇷 Argentina`, `https://github.com/karim-off/XiaBot-Pro`, biobot]
 ], m)
 
 }
