c1c54188f98c8f9b446b9a1cae36238c30c89ec9
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-05-31 00:49:05 -0400

    Actualizar menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -396,8 +396,7 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
 ┃╰───────⧟⭑⧟───────•
 ┗━━━━━━━━━━━━━━━━━━━━━┛`.trim();
     if (m.isGroup) {
-    conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m, null, rpro)
-
+    //conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m, null, rpro)
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
     } else {
