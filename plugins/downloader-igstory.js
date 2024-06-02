437bc00b2a6e3c9b46c1541bb70c9883c0220ec1
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-02 18:07:52 -0400

    Actualizar downloader-igstory.js

--- plugins/downloader-igstory.js
+++ plugins/downloader-igstory.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
   const res = await fetch(`https://api.lolhuman.xyz/api/igstory/${args[0]}?apikey=${lolkeysapi}`);
   const anu = await res.json();
   const anuku = anu.result;
-  if (anuku == '') return m.reply('*[❗] 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙸𝙽𝚅𝙰𝙻𝙸𝙳𝙾 𝙾 𝚂𝙸𝙽 𝙷𝙸𝚂𝚃𝙾𝚁𝙸𝙰𝚂*');
+  if (anuku == '') return m.reply('❱❱ 𝙄 𝙉 𝙁 𝙊 𝙍 𝙈 𝘼 𝘾 𝙄 𝙊 𝙉 ❰❰\n\n🔮 𝘜𝘴𝘶𝘢𝘳𝘪𝘰 𝘪𝘯𝘷𝘢𝘭𝘪𝘥𝘰 𝘰 𝘯𝘰 𝘵𝘪𝘦𝘯𝘦 𝘯𝘪𝘯𝘨𝘶𝘯𝘢 𝘩𝘪𝘴𝘵𝘰𝘳𝘪𝘢 𝘳𝘦𝘤𝘪𝘦𝘯𝘵𝘦');
   for (const i of anuku) {
     const res = await axios.head(i);
     const mime = res.headers['content-type'];
