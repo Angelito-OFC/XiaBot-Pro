968217e3718e0fdfcf55279bf7232bc766af58ff
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-02 10:24:38 -0400

    Actualizar _antiviewonce.js

--- plugins/_antiviewonce.js
+++ plugins/_antiviewonce.js
@@ -12,7 +12,7 @@ export async function before(m, {isAdmin, isBotAdmin}) {
     for await (const chunk of media) {
       buffer = Buffer.concat([buffer, chunk]);
     }
-    const cap = '*𝐃𝐄𝐊𝐔-𝐁𝐎𝐓 te avisa que en este grupo esta prohibido ocultar cosas.*'
+    const cap = '𝙓𝙄𝘼 𝙇𝙊 𝙋𝙐𝙀𝘿𝙀 𝙑𝙀𝙍 𝙏𝙊𝘿𝙊 😂'
     if (/video/.test(type)) {
       return mconn.conn.sendFile(m.chat, buffer, 'error.mp4', `${msg[type].caption ? msg[type].caption + '\n\n' + cap : cap}`, m);
     } else if (/image/.test(type)) {
