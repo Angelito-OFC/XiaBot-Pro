d565430281e9cb9bb002bc46bd0e67767e31ce6b
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-02 11:12:19 -0400

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,25 +1,11 @@
-import fetch from 'node-fetch'
-
-var handler = async (m, { conn, usedPrefix, text, args, command }) => {
-
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let pp = await conn.profilePictureUrl(who).catch(_ => '')
-let name = await conn.getName(who)
-let biografia = await conn.fetchStatus('59168683798' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
-let biografiaBot = await conn.fetchStatus('5493584119436' +'@s.whatsapp.net').catch(_ => 'Sin Biografía')
-let bio = biografia.status?.toString() || 'Sin Biografía'
-let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
-
-await conn.sendContactArray(m.chat, [
-[devnum, `${await conn.getName('59168683798'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `🇧🇴 Bolivia`, `https://www.instagram.com/usxr_angelito`, bio],
-[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 XiaBot-Pro`, `📵 No Hacer Spam`, email, `🇦🇷 Argentina`, `https://github.com/karim-off/XiaBot-Pro`, biobot]
-], m)
+function handler(m) {
+  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
+  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
 
 }
-handler.help = ['owner', 'contacto', 'creador', 'contactos']
-handler.tags = ['info']
-handler.command = /^(owner|contacto|creador|contactos)/i
 
-handler.register = true
+handler.help = ['owner']
+handler.tags = ['main']
+handler.command = ['owner', 'creator', 'creador', 'dueño'] 
 
 export default handler
