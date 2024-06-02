4150cce7504ce8e9d566ef46a63224580246776a
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-02 10:53:05 -0400

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -3,10 +3,10 @@ const handler = async (m, {conn, args}) => {
   const group = m.chat;
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
-      title: '𝙻𝙸𝙽𝙺 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾',
-      body: '𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝',
+      title: '𝙇𝙄𝙉𝙆 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊',
+      body: '𝙓𝙞𝙖 𝘽𝙤𝙩 𝙋𝙧𝙤',
       previewType: 0, thumbnail: fs.readFileSync('./Menu2.jpg'),
-      sourceUrl: `https://github.com/BrunoSobrino/TheMystic-Bot-MD`}}});
+      sourceUrl: `https://www.instagram.com/usxr_angelito`}}});
 };
 handler.help = ['linkgroup'];
 handler.tags = ['group'];
