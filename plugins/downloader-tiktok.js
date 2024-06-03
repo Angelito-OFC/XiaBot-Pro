937c487d5093f20a179693aaba419869a6ffde31
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-03 00:04:26 -0400

    Actualizar downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,53 +1,36 @@
-import fg from 'api-dylux'
-import { tiktokdl } from '@bochilteam/scraper'
+import { tiktokdl } from '@bochilteam/scraper';
+import fg from 'api-dylux';
 
-var handler = async (m, { conn, text, args, usedPrefix, command}) => {
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
-if (!args[0]) return conn.reply(m.chat, `🎌 *Ingrese un enlace de tiktok*\n\nEjemplo, !${command} https://vm.tiktok.com/ZMYG92bUh/`, m, fake, )
-if (!args[0].match(/tiktok/gi)) return conn.reply(m.chat, `🚩 *Verifica que el enlace sea correcto*`, m, fake, )
-
-m.react(rwait)
-
-const { key } = await conn.sendMessage(m.chat, {text: `${wait}`}, {quoted: m})
-await delay(1000 * 1)
-await conn.sendMessage(m.chat, {text: `${waitt}`, edit: key})
-await delay(1000 * 1)
-await conn.sendMessage(m.chat, {text: `${waittt}`, edit: key})
-await delay(1000 * 1)
-await conn.sendMessage(m.chat, {text: `${waitttt}`, edit: key})
-
-try {
-let p = await fg.tiktok(args[0])
-let te = `*Nombre:* ${p.nickname}
-*Usuario:* ${p.unique_id}
-*Duración:* ${p.duration}
-*Descripción:* ${p.description}`
-conn.sendFile(m.chat, p.play, 'tiktok.mp4', te, m)
-m.react(done)
-} catch {
-
-try {
-
-const { author: { nickname }, video, description } = await tiktokdl(args[0])
-const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd
-
-m.react(error)
-if (!url) return conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m, fake, )
-conn.sendFile(m.chat, url, 'fb.mp4', `*Nombre:* ${nickname}\n*Descripción:* ${description}`, m)
-m.react(done)
-} catch {
-m.react(error)
-conn.reply(m.chat, `🚩 *Ocurrió un fallo*`, m, fake, )
-}}
-
+ if (!args[0] && m.quoted && m.quoted.text) {
+  args[0] = m.quoted.text;
 }
-handler.help = ['tiktok']
-handler.tags = ['descargas']
-handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
+if (!args[0] && !m.quoted) throw `Dale el enlace del video Tiktok o cita un enlace de tiktok`;
+ if (!args[0].match(/tiktok/gi)) throw `Verifica el link del tik tok`;
 
-handler.limit = true
-handler.register = true
 
-export default handler
+  let txt = 'Aqui Esta Tu Video';
 
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
+  try {
+    const { author: { nickname }, video, description } = await tiktokdl(args[0]);
+    const url = video.no_watermark2 || video.no_watermark || 'https://tikcdn.net' + video.no_watermark_raw || video.no_watermark_hd;
+
+    if (!url) throw global.error;
+
+    conn.sendFile(m.chat, url, 'tiktok.mp4', '', m);
+  } catch (err) {
+    try {
+      let p = await fg.tiktok(args[0]);
+      conn.sendFile(m.chat, p.play, 'tiktok.mp4', txt, m);
+    } catch {
+      m.reply('*Ocurrio un error*');
+    }
+  }
+};
+
+handler.help = ['tiktok'].map((v) => v + ' <url>');
+handler.tags = ['downloader'];
+handler.command = /^t(t|iktok(d(own(load(er)?)?|l))?|td(own(load(er)?)?|l))$/i;
+
+export default handler;
