60d1fb6d836183b1e0ceeb7d1a0ab32a0008b942
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-03 19:06:41 -0400

    Actualizar Prueba4.js

--- plugins/Prueba4.js
+++ plugins/Prueba4.js
@@ -27,7 +27,7 @@ const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
   const randomIndex = Math.floor(Math.random() * videos.length);
   const randomVideo = videos[randomIndex];
 
-  var messa = await prepareWAMessageMedia({ image: {url: randomVideo.thumbnail}}, { upload: conn.waUploadToServer })
+  var messa = await prepareWAMessageMedia({ image: img, { upload: conn.waUploadToServer })
   const interactiveMessage = {
     body: { text: `-›🧸 𝗥𝗘𝗦𝗨𝗟𝗧𝗔𝗗𝗢𝗦 𝗢𝗕𝗧𝗘𝗡𝗜𝗗𝗢𝗦: ${results.videos.length}\n-›🧸 𝗩𝗜𝗗𝗘𝗢 𝗔𝗟𝗘𝗔𝗧𝗢𝗥𝗜𝗢:\n-›🧸 𝗧𝗜𝗧𝗨𝗟𝗢: ${randomVideo.title}\n-›🧸 𝗔𝗨𝗧𝗢𝗥: ${randomVideo.author.name}\n-›🧸 𝗩𝗜𝗦𝗜𝗧𝗔𝗦: ${randomVideo.views}\n-›🧸 𝗨𝗥𝗟: ${randomVideo.url}\n-›🧸𝗜𝗠𝗔𝗚𝗘𝗡: ${randomVideo.thumbnail}`.trim() },
     footer: { text: `${global.wm}`.trim() },  
