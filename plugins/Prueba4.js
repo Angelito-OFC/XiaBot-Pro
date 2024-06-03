a36123986a532bcca103a47eae04e45e7212308e
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-03 19:10:15 -0400

    Actualizar Prueba4.js

--- plugins/Prueba4.js
+++ plugins/Prueba4.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
  await conn.sendButton2(m.chat, play, mssg.ig, thumbnail, [
     ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
     ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
-  ], null, [['Canal', `${fgcanal}`]], m)
+  ], null, [['Canal', `${md}`]], m)
 }
 handler.help = ['play']
 handler.tags = ['dl']
