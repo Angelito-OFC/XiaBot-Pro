76a6ce40d2b6bf6af6483f534f664e57512ec33e
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-03 22:12:06 -0400

    Actualizar rpg-kalahari.js

--- plugins/rpg-kalahari.js
+++ plugins/rpg-kalahari.js
@@ -1,56 +1,13 @@
-let handler = async (m, { isPrems, conn }) => {
-let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
-if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
+const handler = async (m, {conn, text, command}) => {
+  const yh = global.kalahari;
+  const url = yh[Math.floor(Math.random() * yh.length)];
+  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🔮 KALAHARI 🔮*'}, {quoted: m});
+};
+handler.command = /^(kalahari|mapakalahari)$/i;
+handler.tags = ['internet'];
+handler.help = ['mapa'];
+export default handler;
 
-let img = 'https://cdn.discordapp.com/attachments/1009154096588140605/1157519381258063904/c3ca42a7-cdc2-41e7-8f9c-ad5839c5a986.png?ex=6518e76e&is=651795ee&hm=2497c0f5341ca76564796f47cde68e98889c2e57aabff3e40c4215031c2ff040&'
-let dia = Math.floor(Math.random() * 30)
-let tok = Math.floor(Math.random() * 10)
-let hadesb = Math.floor(Math.random() * 4000)
-let expp = Math.floor(Math.random() * 5000)
-
-  global.db.data.users[m.sender].limit += dia
-  global.db.data.users[m.sender].money += hadesb
-  global.db.data.users[m.sender].joincount += tok
-  global.db.data.users[m.sender].exp += expp
-  
-let texto = `MAPA KALAHARI FREE FIRE 2023`
-
-const fkontak = {
-	"key": {
-    "participants":"0@s.whatsapp.net",
-		"remoteJid": "status@broadcast",
-		"fromMe": false,
-		"id": "Halo"
-	},
-	"message": {
-		"contactMessage": {
-			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-		}
-	},
-	"participant": "0@s.whatsapp.net"
-}
-await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
-//await conn.sendButton(m.chat, texto, wm, img, [['🔰 𝙼𝙴𝙽𝚄', '/menu'] ], fkontak, m)  
-global.db.data.users[m.sender].lastcofre = new Date * 1
-}
-handler.help = ['daily']
-handler.tags = ['xp']
-handler.command = ['mapak', 'mapakalahari', 'kalahari'] 
-handler.register = true
-export default handler
-
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
-
-function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
-
-  hours = (hours < 10) ? "0" + hours : hours
-  minutes = (minutes < 10) ? "0" + minutes : minutes
-  seconds = (seconds < 10) ? "0" + seconds : seconds
-
-  return hours + " Horas " + minutes + " Minutos"
-}
+global.kalahari = [
+  'https://telegra.ph/file/c2ac00af19ad02ae2fddf.jpg',
+];
