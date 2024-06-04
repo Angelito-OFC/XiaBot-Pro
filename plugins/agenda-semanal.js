095bac7a888797a79fd62cb0ec29efaf4340e4be
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-03 21:58:22 -0400

    Actualizar agenda-semanal.js

--- plugins/agenda-semanal.js
+++ plugins/agenda-semanal.js
@@ -1,18 +1,13 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn, command }) => {
-if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
-let url = agenda[Math.floor(Math.random() * pies.length)]
-conn.sendFile(m.chat, url, 'error.jpg', `*🔮 AGENDA SEMANAL 🔮*`, m)
-//conn.sendButton(m.chat, `🔮 AGENDA SEMANAL 🔮`, author, url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
-}
-handler.help = ['agenda']
-handler.tags = ['internet']
-handler.command = /^(agendasemanal)$/
-handler.exp = 50
-handler.level = 0
-export default handler
-
+const handler = async (m, {conn, text, command}) => {
+  const yh = global.agenda;
+  const url = yh[Math.floor(Math.random() * yh.length)];
+  conn.sendMessage(m.chat, {image: {url: url}, caption: '*🔮 AGENDA SEMANAL 🔮*'}, {quoted: m});
+};
+handler.command = /^(agendasemanal|agenda)$/i;
+handler.tags = ['internet'];
+handler.help = ['agenda'];
+export default handler;
 
 global.agenda = [
-"https://telegra.ph/file/8d762cd280250591f4011.jpg",
-]
+  'https://telegra.ph/file/8d762cd280250591f4011.jpg',
+];
