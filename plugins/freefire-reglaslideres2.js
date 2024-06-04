e527c236f3a8b7cb20d44624c173c656742cff0a
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-03 21:40:55 -0400

    Actualizar freefire-reglaslideres2.js

--- plugins/freefire-reglaslideres2.js
+++ plugins/freefire-reglaslideres2.js
@@ -22,7 +22,7 @@ const fkontak = {
 await conn.sendFile(m.chat, img, 'img.jpg', texto, fkontak)
 global.db.data.users[m.sender].lastcofre = new Date * 1
 }
-handler.command = ['reglaslideres2'] 
+handler.command = ['reglaslideres1'] 
 handler.register = true
 handler.admin = true
 export default handler
