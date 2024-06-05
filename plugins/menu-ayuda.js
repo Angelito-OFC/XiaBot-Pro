111791663031d089bb954ecb499c2a17d5836264
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-04 21:31:42 -0400

    Actualizar menu-ayuda.js

---
+++ plugins/menu-ayuda.js
@@ -0,0 +1,63 @@
+import pkg from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto } = pkg
+
+var handler = async (m, { conn, usedPrefix }) => {
+
+let msg = generateWAMessageFromContent(m.chat, {
+  viewOnceMessage: {
+    message: {
+        "messageContextInfo": {
+          "deviceListMetadata": {},
+          "deviceListMetadataVersion": 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({
+            text: ""
+          }),
+          footer: proto.Message.InteractiveMessage.Footer.create({
+            text: ""
+          }),
+          header: proto.Message.InteractiveMessage.Header.create({
+            title: "*HELP MENU*",
+            subtitle: "",
+            hasMediaAttachment: false
+          }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [
+              {
+                "name": "quick_reply",
+                "buttonParamsJson": "{\"display_text\":\".menu\",\"id\":\"message\"}"
+              },
+              {
+                "name": "quick_reply",
+                "buttonParamsJson": "{\"display_text\":\"..menuff\",\"id\":\"message\"}"
+              },
+              {
+                "name": "quick_reply",
+                "buttonParamsJson": "{\"display_text\":\"..menuowner\",\"id\":\"message\"}"
+              },
+              {
+                "name": "quick_reply",
+                "buttonParamsJson": "{\"display_text\":\"..labiblia\",\"id\":\"message\"}"
+              },
+              {
+                "name": "quick_reply",
+                "buttonParamsJson": "{\"display_text\":\"..menuanime\",\"id\":\"message\"}"
+              },
+              {
+                "name": "quick_reply",
+                "buttonParamsJson": "{\"display_text\":\".menuapks\",\"id\":\"message\"}"
+              }
+           ],
+          })
+        })
+    }
+  }
+}, {})
+
+await conn.relayMessage(msg.key.remoteJid, msg.message, { messageId: msg.key.id })
+
+}
+handler.command = /^(help|ayuda)$/i
+
+export default handler
