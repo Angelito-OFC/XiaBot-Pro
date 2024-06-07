a864cf2954126d8e5ef8dd088d55537cf16365e2
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-07 15:07:38 -0400

    Actualizar info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -1,5 +1,24 @@
 var handler = async (m, { conn, command }) => {
 
+let str = `
+
+`
+
+conn.reply(m.chat, str, m, fake, )
+
+}
+handler.help = ['donar', 'donate', 'donasi']
+handler.tags = ['info']
+handler.command = /^(donar|donate|donarxia)i$/i
+
+handler.register = true
+
+export default handler
+
+
+let media = './media/menus/Menu4paypal.jpg'
+let handler = async (m, { conn, command }) => {
+let user = db.data.users[m.sender]
 let str = `🔮 𝘿 𝙊 𝙉 𝘼 𝙍  𝙓 𝙄 𝘼 𝙋 𝙍 𝙊
 
 » 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈
@@ -15,14 +34,33 @@ ${github}
 
 » 𝙂𝙍𝙐𝙋𝙊 𝙊𝙁𝘾
 ${md}`
-
-conn.reply(m.chat, str, m, fake, )
-
-}
-handler.help = ['donar', 'donate', 'donasi']
-handler.tags = ['info']
-handler.command = /^(donar|donate|donarxia)i$/i
-
-handler.register = true
-
+await conn.sendButton(m.chat, str, `𝙓𝙞𝙖𝘽𝙤𝙩-𝙋𝙧𝙤 - 𝘼𝙨𝙞𝙨𝙩𝙚𝙣𝙘𝙞𝙖\nwa.me/59168683798\n\n` + wm, media, [
+['𝙂𝙧𝙪𝙥𝙤𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 🔰', '.grupos'],
+['𝘾𝙧𝙚𝙖𝙙𝙤𝙧𝙖 💗', '#owner'],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ ☘️', '/menu']], null, [
+['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', `https://paypal.me/KatashiF`]], fkontak)}
+/*conn.sendMessage(m.chat, { 
+text: str, 
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: true, 
+mentionedJid:[m.sender],
+"externalAdReply": {
+"showAdAttribution": true,
+"renderLargerThumbnail": true,
+"thumbnail": imagen1, 
+"title": `𝙂𝘼𝙏𝘼 𝘿𝙄𝙊𝙎 - 𝘼𝙎𝙄𝙎𝙏𝙀𝙉𝘾𝙄𝘼`, 
+body: ig, 
+"containsAutoReply": true,
+"mediaType": 1, 
+"mediaUrl": paypal, 
+"sourceUrl": paypal, 
+}}}, { quoted: fkontak })}*/
+/*conn.sendHydrated(m.chat, str, `𝙂𝘼𝙏𝘼 𝘿𝙄𝙊𝙎 - 𝘼𝙎𝙄𝙎𝙏𝙀𝙉𝘾𝙄𝘼\n${asistencia}\n\n` + wm, media, 'https://paypal.me/OficialGD', '🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', null, null, [
+['𝙂𝙧𝙪𝙥𝙤𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝙂𝙧𝙤𝙪𝙥𝙨 🔰', '.grupos'],
+['𝘾𝙧𝙚𝙖𝙙𝙤𝙧𝙖 | 𝘾𝙧𝙚𝙖𝙩𝙤𝙧 💗', '#owner'],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+], m,)}*/
+handler.command = /^dona(te|si)|donar|apoyar|paypal|donating$/i
+handler.exp = 80
 export default handler
