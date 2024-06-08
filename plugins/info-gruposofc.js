5639343429b2cb4cf22da78cac73dd51aec478a6
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-07 23:27:57 -0400

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -5,15 +5,27 @@ const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let text = 
 `
-╭──────>⋆☽⋆ ⋆☾⋆<──────╮
-ㅤ   𝙂𝙐𝙀𝙍𝙍𝘼 𝘿𝙀 𝘾𝙇𝘼𝙉𝙀𝙎
-         ${groupMetadata.subject}
-╰──────>⋆☽⋆ ⋆☾⋆<──────╯
+*ＧＲＵＰＯＳ  ＯＦＣ  ＸＩＡ ＢＯＴ*
 
+𝙒𝙝𝙖𝙩𝙨𝘼𝙥𝙥 𝘾𝙧𝙚𝙖𝙙𝙤𝙧
+
+wa.me/59897246324
+
+𝙎𝙤𝙥𝙤𝙧𝙩𝙚 𝙓𝙞𝙖𝘽𝙤𝙩-𝙋𝙧𝙤
+
+https://chat.whatsapp.com/LcFTUnvu0Tw1tCnA2ybdR6
+
+𝙂𝙡𝙤𝙗𝙖𝙡 𝙓𝙞𝙖𝘽𝙤𝙩-𝙋𝙧𝙤
+
+https://chat.whatsapp.com/J4GaLNqbzAHEkzNQy9N62O
+
+𝘾𝙖𝙣𝙖𝙡 𝙓𝙞𝙖𝘽𝙤𝙩-𝙋𝙧𝙤
+
+https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y
 `.trim()
 
 await conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })   
-//await conn.sendButton(m.chat, wm, text, pp, [[lenguajeGB.smsGI10(), '.on'], [lenguajeGB.smsConMenu(), '/menu']], m, { mentions: [...groupAdmins.map(v => v.id), owner] })
+//await conn.sendButton(m.chat, wm, text, pp, [[lenguajeGB.smsGI10(), '.on'], [lenguajeGB.smsConMenu(), '/menu']], m, { mentions: [...groupAdmins.map(v =>k v.id), owner] })
 }
 handler.help = ['infogrup']
 handler.tags = ['group']
