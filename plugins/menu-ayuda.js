39025d85f4f74a6fcfd5366f1f7a98afa6895396
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-07 22:11:11 -0400

    Actualizar menu-ayuda.js

--- plugins/menu-ayuda.js
+++ plugins/menu-ayuda.js
@@ -23,35 +23,31 @@ let msg = generateWAMessageFromContent(m.chat, {
             hasMediaAttachment: false
           }),
           nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-            buttons: [
-        {
-          name: 'single_select',
-          buttonParamsJson: JSON.stringify({
-            title: 'MENUS XIA',
-            sections: videos.map((video) => ({
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".menu\",\"id\":\"message\"}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".menuff\",\"id\":\"message\"}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".menuowner\",\"id\":\"message\"}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".labiblia\",\"id\":\"message\"}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".menuanime\",\"id\":\"message\"}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".menuapks\",\"id\":\"message\"}"
+const buttonParamsJson = JSON.stringify({
+title: "VER LISTA",
+description: "Infórmate por medios oficiales sobre GataBot",
+sections: [
+{ title: "ℹ️ Información", highlight_label: "Popular",
+rows: [
+{ header: "✅ Redes", title: "🔓 Para: Todos", description: "Infórmate por medios oficiales sobre GataBot", id: usedPrefix + "cuentasgb" },
+{ header: "📢 Grupos/Canales", title: "🔓 Para: Todos", description: "¡Te esperamos!", id: usedPrefix + "grupos" },
+{ header: "🎁 Donar", title: "🔓 Para: Todos", description: "GataBot se mantiene funcionando gracias a donaciones ¡tú también puedes sumarte apoyando el proyecto!", id: usedPrefix + "donar" }
+]},
+{ title: "🔖 Atajos", highlight_label: "Popular",
+rows: [
+{ header: "🆕 Ser Bot (código)", title: "🔓 Para: Todos", description: "¡Conviértete en Bot con el método de código de 8 dígitos!", id: usedPrefix + "serbot --code" },
+{ header: "🤖 Ser Bot (qr)", title: "🔓 Para: Todos", description: "Forma estándar de ser bot con código QR", id: usedPrefix + "serbot" },
+{ header: "🚄 Velocidad", title: "🔓 Para: Todos", description: "Seleccione esto si desea saber el ping del Bot", id: usedPrefix + "ping" },
+{ header: "😺 Estado", title: "🔓 Para: Todos", description: "Conoce en que estado se encuentra GataBot", id: usedPrefix + "estado" }
+]},
+{ title: "Ⓜ️ Menú", highlight_label: "Popular",
+rows: [
+{ header: "⭐ Menú completo", title: "", description: "Visita todos los comandos", id: usedPrefix + "allmenu" }
+]}
+]})
+const interactiveMessage = {
+body: { text: menu },
+footer: { text: wm + ` \nSi algo no funciona utilice el comando *${usedPrefix}menu2*` },
               }
            ],
           })
