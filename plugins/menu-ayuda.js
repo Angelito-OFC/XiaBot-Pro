ab17f04f6d36d402a6d146234339cb3bd216ee32
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-07 21:05:50 -0400

    Actualizar menu-ayuda.js

--- plugins/menu-ayuda.js
+++ plugins/menu-ayuda.js
@@ -1,67 +1,195 @@
-import pkg from '@whiskeysockets/baileys';
-const { generateWAMessageFromContent, proto } = pkg
+import { promises } from 'fs'
+import { join } from 'path'
+import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
 
-var handler = async (m, { conn, usedPrefix }) => {
-
-let msg = generateWAMessageFromContent(m.chat, {
-  viewOnceMessage: {
-    message: {
-        "messageContextInfo": {
-          "deviceListMetadata": {},
-          "deviceListMetadataVersion": 2
-        },
-        interactiveMessage: proto.Message.InteractiveMessage.create({
-          body: proto.Message.InteractiveMessage.Body.create({
-            text: ""
-          }),
-          footer: proto.Message.InteractiveMessage.Footer.create({
-            text: "•🔮𝘾𝙍𝙀𝘼𝘿𝙊𝙍\n•🔮 wa.me/59897246324\n•🔮𝙑𝙀𝙍𝙎𝙄𝙊𝙉 𝘿𝙀𝙇 𝘽𝙊𝙏: 𝟹.𝟸.𝟷\n•🔮𝙁𝙀𝘾𝙃𝘼: ${fecha}"
-          }),
-          header: proto.Message.InteractiveMessage.Header.create({
-            title: "*XIA BOT PRO*",
-            subtitle: "",
-            hasMediaAttachment: false
-          }),
-          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-            buttons: [
-          name: 'single_select',
-          buttonParamsJson: JSON.stringify({
-            title: 'OPCIONES DISPONIBLES',
-            sections: videos.map((video) => ({
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".menu\",\"id\":\"message\"}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".menuff\",\"id\":\"message\"}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".menuowner\",\"id\":\"message\"}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".labiblia\",\"id\":\"message\"}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".menuanime\",\"id\":\"message\"}"
-              },
-              {
-                "name": "quick_reply",
-                "buttonParamsJson": "{\"display_text\":\".menuapks\",\"id\":\"message\"}"
-              }
-           ],
-          })
-        })
-    }
-  }
-}, {})
-
-await conn.relayMessage(msg.key.remoteJid, msg.message, { messageId: msg.key.id })
-
+let tags = {
+  'main': 'ACERCA DE',
+  'bebot': 'SUB BOTS',
+  'game': 'JUEGOS',
+  'econ': 'NIVEL & ECONOMIA',
+  'rg': 'REGISTRO',
+  'sticker': 'STICKER',
+  'img': 'IMAGEN',
+  'maker': 'MAKER',
+  'prem': 'PREMIUM',
+  'group': 'GRUPO',
+  'nable': 'EN/DISABLE OPCIONES', 
+  'nime': 'ANIME',
+  'rnime': 'ANIME REACCION',
+  'dl': 'DESCARGAS',
+  'tools': 'TOOLS',
+  'fun': 'FUN',
+  'cmd': 'DATABASE',
+  'nsfw': 'NSFW +18',
+  'ansfw': 'NSFW ANIME', 
+  'owner': 'OWNER', 
+  'advanced': 'AVANZADO',
 }
-handler.command = /^(help|ayuda)$/i
 
+const defaultMenu = {
+  before: `
+ ◈ ━━━━━ *Senna  ┃ ᴮᴼᵀ* ━━━━━ ◈
+ 
+👋🏻 Hola! *%name*
+👥 Usuarios : %totalreg
+🟢 Tiempo activo : %muptime
+%sbot
+▢ ADD
+• https://instagram.com/fg98_ff
+
+• https://fgmods.xyz
+
+────────────
+%readmore
+  ≡ *LISTA DE MENUS*
+
+Ⓟ = Premium
+ⓓ = Diamantes
+`.trimStart(),
+  header: '┌─⊷ *%category*',
+  body: '▢ %cmd %isdiamond %isPremium',
+  footer: '└───────────\n',
+  after: '',
+}
+
+let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
+ try {
+    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+    let { exp, star, level } = global.db.data.users[m.sender]
+    let { min, xp, max } = xpRange(level, global.multiplier)
+    let name = await conn.getName(m.sender)
+    let d = new Date(new Date + 3600000)
+    let locale = 'es'
+    // d.getTimeZoneOffset()
+    // Offset -420 is 18.00
+    // Offset    0 is  0.00
+    // Offset  420 is  7.00
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }).format(d)
+    let time = d.toLocaleTimeString(locale, {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
+    let _uptime = process.uptime() * 1000
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+    let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
+      return {
+        help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
+        tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+        prefix: 'customPrefix' in plugin,
+        star: plugin.star,
+        premium: plugin.premium,
+        enabled: !plugin.disabled,
+      }
+    })
+    for (let plugin of help)
+      if (plugin && 'tags' in plugin)
+        for (let tag of plugin.tags)
+          if (!(tag in tags) && tag) tags[tag] = tag
+    conn.menu = conn.menu ? conn.menu : {}
+    let before = conn.menu.before || defaultMenu.before
+    let header = conn.menu.header || defaultMenu.header
+    let body = conn.menu.body || defaultMenu.body
+    let footer = conn.menu.footer || defaultMenu.footer
+    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : ``) + defaultMenu.after
+    let _text = [
+      before,
+      ...Object.keys(tags).map(tag => {
+        return header.replace(/%category/g, tags[tag]) + '\n' + [
+          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
+            return menu.help.map(help => {
+              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
+                .replace(/%isstar/g, menu.star ? '˄' : '')
+                .replace(/%isPremium/g, menu.premium ? '˄' : '')
+                .trim()
+            }).join('\n')
+          }),
+          footer
+        ].join('\n')
+      }),
+      after
+    ].join('\n')
+    let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
+    let replace = {
+      '%': '%',
+      p: _p, uptime, muptime,
+      taguser: '@' + m.sender.split("@s.whatsapp.net")[0],
+      wasp: '@0',
+      me: conn.getName(conn.user.jid),
+      npmname: _package.name,
+      version: _package.version,
+      npmdesc: _package.description,
+      npmmain: _package.main,
+      author: _package.author.name,
+      license: _package.license,
+      exp: exp - min,
+      maxexp: xp,
+      totalexp: exp,
+      xp4levelup: max - exp,
+      github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
+      level, star, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg,
+      readmore: readMore
+    }
+    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+
+    let pp = 'https://telegra.ph/file/4c3e4b782c82511b3874d.mp4'
+    let pp2 = 'https://telegra.ph/file/d8c5e18ab0cfc10511f63.mp4'
+    let pp3 = 'https://telegra.ph/file/96e471a87971e2fb4955f.mp4'
+    let pp4 = 'https://telegra.ph/file/09b920486c3c291f5a9e6.mp4'
+    m.react('💻')
+
+  //  conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: m })
+let listSections = []    
+listSections.push({
+title: '',
+rows: [{ header: "🔮 Menu Completo", title: "", id: `.allmenu`, description: `Para ver todos los comandos de XiaBot\n` }, { header: "🔞 Menu Hot", title: "", id: `.labiblia`, description: `Para ver el menú hot de Xia 🔥\n` },
+{ header: "🎮 Menu Juegos", title: "", id: `.menugame`, description: `Para ver el menu de juegos\n` },
+{ header: "🔫 Menú FreeFire", title: "", id: `.menuff`, description: `Para ver el menú de Freefire\n` },
+{ header: "🫅🏻 Creador", title: "", id: `.owner`, description: `comunicate con mi creador ⚙️` }
+]})
+await conn.sendList(m.chat, '👋🏻 Hola¡! Bienvenido A Mi Menú\n\n*Creador:* Angelito\n*Versión:* 3.3.0\n\n🔮 si hay algún error puedes reportarlo, usando el comando: #reporte\n\nGracias¡! ✨', null, `𝙈𝙀𝙉𝙐𝙎 𝙓𝙄𝘼`, listSections, { mentions: [m.sender]}, {quoted: m})
+  } catch (e) {
+    conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error.', m)
+    throw e
+  }
+}
+
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = ['menu', 'help', 'menú'] 
+handler.register = true 
 export default handler
+
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+      }
