8cbcf3f46b814e053b70cd8479bcaf5c45b31837
Karim-off <153532713+Karim-off@users.noreply.github.com>
2024-06-08 22:23:37 -0400

    Actualizar downloader-pixiv.js

--- plugins/downloader-pixiv.js
+++ plugins/downloader-pixiv.js
@@ -1,22 +1,22 @@
-/*import { URL_REGEX } from '@adiwajshing/baileys'
+import { URL_REGEX } from '@whiskeysockets/baileys'
 import { fileTypeFromBuffer } from 'file-type'
 import { Pixiv } from '@ibaraki-douji/pixivts'
 const pixiv = new Pixiv()
 
 let handler = async (m, { conn, text }) => {
-	if (!text) throw 'Input Pixiv Url'
+	if (!text) throw 'Masukan Query Atau Url Pixiv'
 	let res = await pixivDl(text)
-	await m.reply('_In progress, please wait..._')
+	await m.reply(wait)
 	for (let i = 0; i < res.media.length; i++) {
-		let caption = i == 0 ? `${res.caption}\n\n*âœï¸ By:* ${res.artist}\n*ðŸ”– Tags:* ${res.tags.join(', ')}` : ''
+		let caption = i == 0 ? `${res.caption}\n\n*By:* ${res.artist}\n*Tags:* ${res.tags.join(', ')}` : ''
 		let mime = (await fileTypeFromBuffer(res.media[i])).mime 
 		await conn.sendMessage(m.chat, { [mime.split('/')[0]]: res.media[i], caption, mimetype: mime }, { quoted: m })
 	}
 }
-handler.help = ['pixiv']
+handler.help = handler.alias = ['pixiv']
 handler.tags = ['downloader']
 handler.command = /^(pixiv)$/i
-
+handler.limit = true
 export default handler
 
 async function pixivDl(query) {
@@ -24,7 +24,7 @@ async function pixivDl(query) {
 		if (!/https:\/\/www.pixiv.net\/en\/artworks\/[0-9]+/i.test(query)) throw 'Invalid Pixiv Url'
 		query = query.replace(/\D/g, '')
 		let res = await pixiv.getIllustByID(query).catch(() => null)
-		if (!res) throw `ID "${query}" not found :/`
+		if (!res) throw `Pencarian "${query}" Tidak Ditemukan`
 		let media = []
 		for (let x = 0; x < res.urls.length; x++) media.push(await pixiv.download(new URL(res.urls[x].original)))
 		return {
@@ -32,7 +32,7 @@ async function pixivDl(query) {
 		}
 	} else {
 		let res = await pixiv.getIllustsByTag(query)
-		if (!res.length) throw `Tag's "${query}" not found :/`
+		if (!res.length) throw `Pencarian "${query}" Tidak Di Temukan`
 		res = res[~~(Math.random() * res.length)].id
 		res = await pixiv.getIllustByID(res)
 		let media = []
@@ -42,4 +42,3 @@ async function pixivDl(query) {
 		}
 	}
 }
-*/
